2c7aa052abc42bf26aca7458e8240f4bf0d323d6
Chathura Kulathunga <chathurakulathunga.foxc@gmail.com>
2026-06-01 23:02:54 +0530

    Add sanity cover images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
 
 export default defineConfig({
   site: 'https://chathura-kulathunga.github.io',
+  integrations: [
+    sanity({
+      projectId: 'xsg8551p',
+      dataset: 'production',
+      useCdn: false, 
+      apiVersion: '2024-05-30', 
+    }),
+  ],
 });
